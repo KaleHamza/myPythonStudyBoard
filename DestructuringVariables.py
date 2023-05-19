6e09ac9340252587de68df9bff5d01448e5ebbee
Hamza Kale <71084430+KaleHamza@users.noreply.github.com>
2023-05-19 21:01:05 +0300

    Create DestructuringVariables.py

---
+++ DestructuringVariables.py
@@ -0,0 +1,13 @@
+person = ("Bob", 42, "Mechanic")
+name, _,profession = person
+print(name, profession)
+
+################################
+head, *tail = [1, 2, 3, 4, 5]
+print(head)
+print(tail)
+
+#################################
+
+people = [("Hamza", 33, "Bilgisayar"), ("Mahmut", 34, "Makine"), ("Ayşe", 32, "Öğretmenlik")]
+  print(f"Name: {person[0]}, Age: {person[1]}, Profession: {person[2]}")
