b0e7aa58d3dc580dcc8a71743d8252146c615419
Hamza Kale <71084430+KaleHamza@users.noreply.github.com>
2023-05-19 21:19:21 +0300

    Create FunctionsReturningValues.py

---
+++ FunctionsReturningValues.py
@@ -0,0 +1,12 @@
+def add(x, y = 5):
+  print(x + y)
+add(5)#working clearly because about the fonc. default parameter
+
+
+####### Return value 
+def add(x, y):
+  return x + y
+  print(x + y)#Never get called..
+
+result = add(3, 4)
+print(result)
