080c357328db076f691064d4bd34ae24daede398
Hamza Kale <71084430+KaleHamza@users.noreply.github.com>
2023-05-20 00:51:20 +0300

    Update DictionaryComprehensions.py

--- DictionaryComprehensions.py
+++ DictionaryComprehensions.py
@@ -4,5 +4,21 @@ users = [
   (2, "Zuka", "456")
   (3, "Ddeneme", "789")
 ]
+                    #            #
+username_mapping = {user[1]: user
+                    for user in users}
 
-username_mapping = 
+print(username_mapping["Hamza"])
+
+for user in users:
+  if user[1] == "Hamza":
+    print(user)
+  
+username_input = input("username:")
+password_input = input("password:")
+_, username, password = username_mapping[username_input]
+
+if password_input == password:
+  print("Correct")
+else:
+  print("False")
